ba48bdc4c85e36f8776311ced53871ebc3ac14f8
yousa25 <2446814668@qq.com>
2026-06-08 17:57:26 +0800

    类，对象，类成员简介

---
+++ CSharp/Mystia/ClassAndInstance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+
+
+namespace ClassAndInstance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //(new Form()).Text="My Form";
+            //(new Form()).ShowDialog();
+
+          
+
+            Timer timer = new Timer();
+            timer.Interval = 1*1000;
+            timer.Elapsed += Timer_Elapsed; ;
+            timer.Start();
+
+            Console.ReadLine();
+
+
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+
+    }
+}
