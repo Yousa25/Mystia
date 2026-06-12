9646e30da6421d487cad5d285578a1f444b25b08
yousa25 <2446814668@qq.com>
2026-06-12 17:44:28 +0800

    # 详解类型、变量与对象

---
+++ CSharp/Mystia/TypeSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypeSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type maType = typeof(Form);
+            PropertyInfo[] properties = maType.GetProperties();
+            MethodInfo[] methodInfos = maType.GetMethods();
+            EventInfo[] eventInfos = maType.GetEvents();
+            
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.Name);
+            }
+            foreach (MethodInfo method in methodInfos)
+            {
+                Console.WriteLine(method.Name);
+            }
+
+        }
+    }
+}
